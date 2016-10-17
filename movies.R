467b009acf28f156ea34c0f5c293cf54158295aa
Mayank Thirani <mthirani@dons.usfca.edu>
2016-10-17 15:42:35 -0700

    Updated R code with analysis

--- movies.R
+++ movies.R
@@ -1,8 +1,6 @@
 # Loading the sufficient dataset and removing the NA values
 
 library(MASS)
-rm(moviesdataset)
-rm(imdb_rating)
 movies = read.csv("movies.csv", na.strings = "?")
 movies = na.omit(movies)
 attach(movies)
@@ -32,10 +30,35 @@ percent70data = round(0.70*totalrows)
 percent30data = totalrows - percent70data
 train = 1:percent70data
 test = -train
-moviesTrain = movies[train, ]
-moviesTest = movies[-train, ]
+moviesTrain = moviesdataset[train, ]
+moviesTest = moviesdataset[-train, ]
 dim(moviesTrain)
 dim(moviesTest)
 
+# Plots with all predictors
+
+attach(moviesdataset)
+plot(imdb_score, num_critic_for_reviews)
+plot(imdb_score, director_facebook_likes)
+plot(imdb_score, gross)
+plot(imdb_score, num_voted_users)
+plot(imdb_score, facenumber_in_poster)
+plot(imdb_score, num_user_for_reviews)
+plot(country, imdb_score)
+plot(imdb_score, movie_facebook_likes)
+# plot(imdb_score, budget); Impacting not at all
+# plot(imdb_score, cast_total_facebook_likes); Impacting very few
+# plot(imdb_score, duration); Impacting not at all
+# plot(imdb_score, actor_2_facebook_likes); Impacting very few
+# plot(imdb_score, actor_1_facebook_likes); Impacting not at all
+# plot(imdb_score, actor_3_facebook_likes); Impacting not at all
+# plot(color, imdb_score); Impacting not at all
+# plot(director_name, imdb_score); Impacting not at all
+
 # As our target has been classified into more than 2 levels so we are using a stabilized statistical learning method, LDA
 
+require(ISLR)
+require(class)
+require(boot)
+dim(moviesdataset)
+lda.fit = lda(imdb_rating~num_critic_for_reviews + director_facebook_likes + gross + num_voted_users + facenumber_in_poster + num_user_for_reviews + country + movie_facebook_likes, data=moviesTrain)
